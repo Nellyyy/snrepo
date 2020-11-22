fda5b57fbc1fa8108459dc54f7d6ffc3683f12df
Camille <camille.megy.36@gmail.com>
2020-11-22 15:52:59 +0100

    schéma différences finies
    fonction pour passer du temps t au temps t+1

--- Sciences_numériques.py
+++ Sciences_numériques.py
@@ -12,11 +12,11 @@ import numpy as np
 #définitions des différents paramètres
 #(pourra être demandé à l'utilisateur plus tard)
 
-Lx=0,2  #longueur de la plaque selon x (en m)
-Ly=0,1  #longueur de la plaque selon y (en m)
+Lx=0.2  #longueur de la plaque selon x (en m)
+Ly=0.1  #longueur de la plaque selon y (en m)
 Px=5    #nombre de points du maillage selon x
 Py=6    #nombre de points du maillage selon y
-a=98*(10**6) #diffusivité thermique pour une plaque en silicium
+a=98*(10**-6) #diffusivité thermique pour une plaque en silicium
 
 Ttot=10 #temps total de l'expérience en seconde
 Pt=100  #maillage temporel 
@@ -39,12 +39,40 @@ Temp_inte_0=np.ones((Px-2,Py))*U0
 for k in range(Px-2):
     Temp_inte_0[k,0]=U1     #conditions aux limites en y=0
     Temp_inte_0[k,Py-1]=U2  #conditions aux limites en y=Ly
-    
+
 #on combine Temp_inte_0 aux conditions aux limites x=0 et x=Lx
 Temp_0=np.vstack((Cote0,Temp_inte_0,CoteLx))
 
-#on fait pour les points (0,0) et (Lx,0) une moyenne entre U2 et U1 pour "modéliser"
+#on fait pour les points (0,0) et (Lx,0) une moyenne entre U2 et U1 pour <<modéliser>>
 #la continuité de la température aux bords
 Temp_0[0,0]=Temp_0[Px-1,0]=(U1+U2)/2
+
+############################################################################
+
+#schémas aux différences finies
+#déterminer le profil de température à l'instant i+1 en fonction de celui au temps i
+
+Dt=Ttot/Pt
+Dx=Lx/Px
+Dy=Ly/Py
+
+A=a*Dt/Dx**2
+B=a*Dt/Dy**2
+
+def differences_finies(Temp_i): #argument : profil de température au temps i
+    #on note Temp_j la température au temps i+1
+    #conditions aux limites de Temp_j avant de faire les calculs intérieurs
+    Temp_j=np.vstack((Cote0,np.zeros((Px-2,Py)),CoteLx))
+    Temp_j[0,0]=Temp_j[Px-1,0]=(U1+U2)/2
+    
+    for k in range(1, Px-1):
+        Temp_j[k,0]=U1     #conditions aux limites en y=0
+        Temp_j[k,Py-1]=U2  #conditions aux limites en y=Ly
+        for h in range(1, Py-1):
+            Temp_j[k,h]=(1-2*(A+B))*Temp_i[k,h]+A*(Temp_i[k+1,h]+Temp_i[k-1,h])+B*(Temp_i[k,h+1]+Temp_i[k,h-1]) 
+   
+    return Temp_j
+    
+#print(differences_finies(Temp_0))
 
 ############################################################################
