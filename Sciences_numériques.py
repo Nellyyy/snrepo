dbb6d6fa30f9bb7671a517a1501914f99d8031da
Camille <camille.megy.36@gmail.com>
2020-11-22 14:58:21 +0100

    Définition des paramètres initiaux
    pourra être demandé dans un fichier annexe plus tard

---
+++ Sciences_numériques.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 22 14:51:07 2020
+
+@author: camil
+"""
+
+#projet sciences numériques
+
+#définitions des différents paramètres
+#(pourra être demandé à l'utilisateur plus tard)
+
+Lx=0,2  #longueur de la plaque selon x (en m)
+Ly=0,1  #longueur de la plaque selon y (en m)
+Px=5    #nombre de points du maillage selon x
+Py=6    #nombre de points du maillage selon y
+a=98*(10**6) #diffusivité thermique pour une plaque en silicium
+
+Ttot=10 #temps total de l'expérience en seconde
+Pt=100  #maillage temporel 
+
+U0=294  #température initiale en K
+U1=304  #température à la limite T1
+U2=314  #température à la limite T2
+
+
