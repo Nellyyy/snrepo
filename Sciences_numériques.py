cec99ca62259323ceaf772021f4f773b6886cc55
Camille <camille.megy.36@gmail.com>
2020-11-22 16:30:28 +0100

    Réecriture plus propre des fonctions
    J'ai réécrit les conditions initiales et aux limites sous forme de fonction, ça sera plus simple de les utiliser plus tard

--- Sciences_numériques.py
+++ Sciences_numériques.py
@@ -28,41 +28,48 @@ U2=314  #température à la limite T2
 ############################################################################
 
 #conditions aux limites
-Cote0=np.ones((1,Py))*U2  #profil de température sur le côté tel que x=0
-CoteLx=np.ones((1,Py))*U2 #profil de température sur le côté tel que x=Lx
+def condition_limite_y(Py,U1,U2):
+    Cote=np.ones((1,Py))*U2  #profil sur les côtés x=O et x=Lx
+    return Cote
 
 #Profil de température à t=0
 #on crée un tableau pour les températures intérieures, ie sans prendre en compte
 #les côtés x=0 et x=Lx, calculés ci-dessus, qui seront ensuite rajoutés
 
-Temp_inte_0=np.ones((Px-2,Py))*U0
-for k in range(Px-2):
-    Temp_inte_0[k,0]=U1     #conditions aux limites en y=0
-    Temp_inte_0[k,Py-1]=U2  #conditions aux limites en y=Ly
+def temperature_initiale(Px,Py,U0,U1,U2):
+    Temp_inte_0=np.ones((Px-2,Py))*U0
+    for k in range(Px-2):
+        Temp_inte_0[k,0]=U1     #conditions aux limites en y=0
+        Temp_inte_0[k,Py-1]=U2  #conditions aux limites en y=Ly
+    #on combine Temp_inte_0 aux conditions aux limites x=0 et x=Lx
+    Cote=condition_limite_y(Py,U1,U2)
+    Temp_0=np.vstack((Cote,Temp_inte_0,Cote))
+    #on fait pour les points (0,0) et (Lx,0) une moyenne entre U2 et U1 pour <<modéliser>>
+    #la continuité de la température aux bords
+    Temp_0[0,0]=Temp_0[Px-1,0]=(U1+U2)/2
+    return Temp_0
 
-#on combine Temp_inte_0 aux conditions aux limites x=0 et x=Lx
-Temp_0=np.vstack((Cote0,Temp_inte_0,CoteLx))
-
-#on fait pour les points (0,0) et (Lx,0) une moyenne entre U2 et U1 pour <<modéliser>>
-#la continuité de la température aux bords
-Temp_0[0,0]=Temp_0[Px-1,0]=(U1+U2)/2
+#test
+#print(temperature_initiale(Px,Py,U0,U1,U2))
 
 ############################################################################
 
 #schémas aux différences finies
 #déterminer le profil de température à l'instant i+1 en fonction de celui au temps i
 
-Dt=Ttot/Pt
-Dx=Lx/Px
-Dy=Ly/Py
+def differences_finies(Temp_i,Lx,Ly,Px,Py,a,U0,U1,U2,Ttot,Pt): #Temp_i : profil de température au temps i
+    Dt=Ttot/Pt
+    Dx=Lx/Px
+    Dy=Ly/Py
+    
+    A=a*Dt/Dx**2
+    B=a*Dt/Dy**2
+    
+    Cote=condition_limite_y(Py,U1,U2)
 
-A=a*Dt/Dx**2
-B=a*Dt/Dy**2
-
-def differences_finies(Temp_i): #argument : profil de température au temps i
     #on note Temp_j la température au temps i+1
     #conditions aux limites de Temp_j avant de faire les calculs intérieurs
-    Temp_j=np.vstack((Cote0,np.zeros((Px-2,Py)),CoteLx))
+    Temp_j=np.vstack((Cote,np.zeros((Px-2,Py)),Cote))
     Temp_j[0,0]=Temp_j[Px-1,0]=(U1+U2)/2
     
     for k in range(1, Px-1):
@@ -72,7 +79,8 @@ def differences_finies(Temp_i): #argument : profil de température au temps i
             Temp_j[k,h]=(1-2*(A+B))*Temp_i[k,h]+A*(Temp_i[k+1,h]+Temp_i[k-1,h])+B*(Temp_i[k,h+1]+Temp_i[k,h-1]) 
    
     return Temp_j
-    
-#print(differences_finies(Temp_0))
+
+#test
+#print(differences_finies(temperature_initiale(Px,Py,U0,U1,U2),Lx,Ly,Px,Py,a,U0,U1,U2,Ttot,Pt))
 
 ############################################################################
