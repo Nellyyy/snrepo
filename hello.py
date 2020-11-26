c1936f3c394ca36a869e77c0ca2f1576f2e52edd
Nelly <lyla1998@gmail.com>
2020-11-26 15:47:49 +0100

    changement unité
    attention passage de m à cm + gestion de l'exception

--- hello.py
+++ hello.py
@@ -2,14 +2,17 @@
 from math import *
 import numpy as np
 
+import matplotlib.pyplot as plt 
+from matplotlib.colors import LogNorm 
+
 #définitions des différents paramètres
 
 U0=294  #température initiale en K
 U1=304  #température à la limite T1
 U2=314  #température à la limite T2
 
-Lx=0.2  #longueur de la plaque selon x (en m)
-Ly=0.1  #longueur de la plaque selon y (en m)
+Lx=200  #longueur de la plaque selon x (en cm)
+Ly=100  #longueur de la plaque selon y (en cm)
 Px=5    #nombre de points du maillage selon x
 Py=6    #nombre de points du maillage selon y
 
@@ -25,14 +28,32 @@ def saisie_n():
 def solution_analytique(U1,U2,Lx,Ly,Px,Py):
     n = saisie_n()
     global somme
-    Temp_i=np.ones((Px,Py))
-    for i in range(1,Px):
-        for j in range(1,Py):
+    Temp_i=np.zeros((Px,Py))
+    for i in range(0,Px):
+        for j in range(0,Py):
             for k in range(0,n):
-                somme += (1/(2*n+1))*sin(((2*n+1)*pi*i)/Lx)*(sinh((Ly-j)*(2*n*+1)*pi/Lx))/(sinh(((2*n*+1)*pi*Ly)/Lx))
+                try:
+                    somme += (1/(2*n+1))*sin(((2*n+1)*pi*i)/Lx)*(sinh(((Ly-j)*(2*n*+1)*pi)/Lx))/(sinh(((2*n*+1)*pi*Ly)/Lx))
+                except OverflowError:
+                    somme = float('inf')
             Temp_i[i,j]=U2+((4*(U1-U2))/pi)*somme
-            print("i=",i," j=",j, "tabb= ",Temp_i[i,j])
+            print("i=",i," j=",j, "tab= ",Temp_i[i,j])
     return Temp_i
 
-solution_analytique(U1,U2,Lx,Ly,Px,Py)
+#affichage du profil de température
+#ajouter le ss programme de Camille sur la stabilité en fct de comment on organise les modules
+
+def affichage_profil(U1,U2,Lx,Ly,Px,Py):
+    #si stabilité
+    Temp_i = solution_analytique(U1,U2,Lx,Ly,Px,Py)
+    mini=np.min([U0,U1,U2])
+    maxi=np.max([U0,U1,U2])
+    with open("solana.txt", "w") as filout:
+        filout.write("{}\n".format(Temp_i))
+        plt.pcolormesh(Temp_i, cmap=plt.cm.Oranges, vmin=mini, vmax=maxi) 
+        plt.show() 
+    return Temp_i
+
+affichage_profil(U1,U2,Lx,Ly,Px,Py)
+
 
