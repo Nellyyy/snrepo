6732b78a521b907b00602ec406abdb19fc58e29a
Camille <camille.megy.36@gmail.com>
2020-11-29 19:01:33 +0100

    Update maillage + axe y et y
    - inversion entre les axes x et y pour obtenir ce qui est attendu dans l'énoncé
    - seul le dernier maillage est affiché

--- Sciences_numériques.py
+++ Sciences_numériques.py
@@ -14,14 +14,14 @@ from matplotlib.colors import LogNorm
 #définitions des différents paramètres
 #(pourra être demandé à l'utilisateur plus tard)
 
-Lx=0.2  #longueur de la plaque selon x (en m)
-Ly=0.1  #longueur de la plaque selon y (en m)
-Px=5    #nombre de points du maillage selon x
-Py=6    #nombre de points du maillage selon y
+Lx=1  #longueur de la plaque selon x (en m)
+Ly=1  #longueur de la plaque selon y (en m)
+Px=80    #nombre de points du maillage selon x
+Py=100    #nombre de points du maillage selon y
 a=98*(10**-6) #diffusivité thermique pour une plaque en silicium
 
-Ttot=100 #temps total de l'expérience en seconde
-Pt=100  #maillage temporel 
+Ttot=1000 #temps total de l'expérience en seconde
+Pt=5000  #maillage temporel 
 
 U0=294  #température initiale en K
 U1=304  #température à la limite T1
@@ -55,25 +55,27 @@ def stabilite_schema(a,Lx,Ly,Px,Py,Ttot,Pt):
 ############################################################################
 
 #conditions aux limites
-def condition_limite_y(Py,U1,U2):
-    Cote=np.ones((1,Py))*U2  #profil sur les côtés x=O et x=Lx
-    return Cote
+def condition_limite_x(Px,U1,U2):
+    Cote_0=np.ones((1,Px))*U1  #profil sur les côtés y=O
+    Cote_Ly=np.ones((1,Px))*U2  #profil sur les côtés y=Ly
+    return Cote_0,Cote_Ly
 
 #Profil de température à t=0
 #on crée un tableau pour les températures intérieures, ie sans prendre en compte
 #les côtés x=0 et x=Lx, calculés ci-dessus, qui seront ensuite rajoutés
 
 def temperature_initiale(Px,Py,U0,U1,U2):
-    Temp_inte_0=np.ones((Px-2,Py))*U0
-    for k in range(Px-2):
-        Temp_inte_0[k,0]=U1     #conditions aux limites en y=0
-        Temp_inte_0[k,Py-1]=U2  #conditions aux limites en y=Ly
-    #on combine Temp_inte_0 aux conditions aux limites x=0 et x=Lx
-    Cote=condition_limite_y(Py,U1,U2)
-    Temp_0=np.vstack((Cote,Temp_inte_0,Cote))
+    Temp_inte_0=np.ones((Py-2,Px))*U0
+    for k in range(Py-2):
+        Temp_inte_0[k,0]=U2     #conditions aux limites en x=0
+        Temp_inte_0[k,Px-1]=U2  #conditions aux limites en x=Lx
+    #on combine Temp_inte_0 aux conditions aux limites y=0 et y=Ly
+    Cote_0=condition_limite_x(Px,U1,U2)[0]
+    Cote_Ly=condition_limite_x(Px,U1,U2)[1]
+    Temp_0=np.vstack((Cote_0,Temp_inte_0,Cote_Ly))
     #on fait pour les points (0,0) et (Lx,0) une moyenne entre U2 et U1 pour <<modéliser>>
     #la continuité de la température aux bords
-    Temp_0[0,0]=Temp_0[Px-1,0]=(U1+U2)/2
+    Temp_0[0,0]=Temp_0[0,Px-1]=(U1+U2)/2
     return Temp_0
 
 #test
@@ -92,18 +94,19 @@ def differences_finies(Temp_i,Lx,Ly,Px,Py,a,U0,U1,U2,Ttot,Pt): #Temp_i : profil 
     A=a*Dt/Dx**2
     B=a*Dt/Dy**2
     
-    Cote=condition_limite_y(Py,U1,U2)
+    Cote_0=condition_limite_x(Px,U1,U2)[0]
+    Cote_Lx=condition_limite_x(Px,U1,U2)[1]
 
     #on note Temp_j la température au temps i+1
     #conditions aux limites de Temp_j avant de faire les calculs intérieurs
-    Temp_j=np.vstack((Cote,np.zeros((Px-2,Py)),Cote))
-    Temp_j[0,0]=Temp_j[Px-1,0]=(U1+U2)/2
+    Temp_j=np.vstack((Cote_0,np.zeros((Py-2,Px)),Cote_Lx))
+    Temp_j[0,0]=Temp_j[0,Px-1]=(U1+U2)/2
     
-    for k in range(1, Px-1):
-        Temp_j[k,0]=U1     #conditions aux limites en y=0
-        Temp_j[k,Py-1]=U2  #conditions aux limites en y=Ly
-        for h in range(1, Py-1):
-            Temp_j[k,h]=(1-2*(A+B))*Temp_i[k,h]+A*(Temp_i[k+1,h]+Temp_i[k-1,h])+B*(Temp_i[k,h+1]+Temp_i[k,h-1]) 
+    for k in range(1, Py-1):
+        Temp_j[k,0]=U2     #conditions aux limites en x=0
+        Temp_j[k,Px-1]=U2  #conditions aux limites en x=Lx
+        for h in range(1, Px-1):
+            Temp_j[k,h]=(1-2*(A+B))*Temp_i[k,h]+B*(Temp_i[k+1,h]+Temp_i[k-1,h])+A*(Temp_i[k,h+1]+Temp_i[k,h-1]) 
    
     return Temp_j
 
@@ -114,9 +117,7 @@ def differences_finies(Temp_i,Lx,Ly,Px,Py,a,U0,U1,U2,Ttot,Pt): #Temp_i : profil 
 
 #obtention du maillage de température à chaque instant
 #les différents maillages sont conservés dans un fichier txt nommé maillage_temp
-#on affiche pour chaque point Pt le maillage de température par des nuances oranges
-#A VOIR : lorsqu'on prend bcp de points, ça met bcp de temps : essayer d'afficher
-#seulement tous les 5 points le schéma par ex
+#on affiche le maillage de température à la fin de l'expérience par des nuances oranges
 
 def profil_temperature(Lx,Ly,Px,Py,a,U0,U1,U2,Ttot,Pt):
     if stabilite_schema(a,Lx,Ly,Px,Py,Ttot,Pt)==1:
@@ -126,12 +127,15 @@ def profil_temperature(Lx,Ly,Px,Py,a,U0,U1,U2,Ttot,Pt):
         with open("temperature.txt", "w") as filout:
             for t in range(Pt):
                 filout.write("{}\n".format(Temp_i))
-                plt.pcolormesh(Temp_i, cmap=plt.cm.Oranges, vmin=mini, vmax=maxi) 
-                plt.show() 
                 Temp_i=differences_finies(Temp_i,Lx,Ly,Px,Py,a,U0,U1,U2,Ttot,Pt)
-    return Temp_i
+            plt.pcolormesh(Temp_i, cmap=plt.cm.Oranges, vmin=mini, vmax=maxi) 
+            plt.show()
+            return Temp_i
+    else:
+        print("schéma non cohérent")
+    
 
 #test    
-#profil_temperature(Lx,Ly,Px,Py,a,U0,U1,U2,Ttot,Pt)
+print(profil_temperature(Lx,Ly,Px,Py,a,U0,U1,U2,Ttot,Pt))
 
 ############################################################################
